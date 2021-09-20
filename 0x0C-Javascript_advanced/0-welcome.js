27269a5e19cd45512eec454494aed271c4874ee4
CSant04y <esquivelcarlos27893@gmail.com>
2021-09-20 16:14:03 +0000

    added function that has lexical outer scope

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+
+function welcome(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+
+    function displayFullName() {
+        return alert("Welcome " + fullName + "!")
+    }
+
+    displayFullName()
+}
